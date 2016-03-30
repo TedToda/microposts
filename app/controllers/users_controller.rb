fcd93ed8b59f68430e78eb1ce0fc9547fd087b59
tedtoda <teppei.toda@gmail.com>
2016-03-30 12:50:18 +0000

    tochu commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,34 @@ class UsersController < ApplicationController
     end
   end
 
+  #def edit
+   # @session_id = session[:user_id]
+	#if current_user == @user
+	#	if @user.update(user_params)
+  #	flash[:success] = “updated”
+   # redirect_to root_path
+    #else
+    #flash.now[:alert] = “false”
+    #render ‘edit’
+    #end
+  #end
+   # else
+    #flash[:danger] = “login as a correct user”
+    #redirect_to login_url
+    #end
+  #end
+
+
+  def update
+    if @user.update(user_params)
+      # 保存に成功した場合はトップページへリダイレクト
+      redirect_to root_path , notice: 'updated'
+    else
+      # 保存に失敗した場合は編集画面へ戻す
+      render 'edit'
+    end
+  end
+  
   private
 
   def user_params
